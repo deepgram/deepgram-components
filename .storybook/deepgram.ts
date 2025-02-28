048f90a8eed86de96d0338d75505cbc88a3464b6
Luke Oliff <luke@lukeoliff.com>
2025-02-28 15:34:16 +0000

    feat: better readme, preview image

---
+++ .storybook/deepgram.ts
@@ -0,0 +1,9 @@
+import { create } from '@storybook/theming';
+
+export default create({
+  base: 'light',
+  brandTitle: 'Deepgram Storybook',
+  brandUrl: 'https://deepgram.com',
+  brandImage: 'https://www.datocms-assets.com/96965/1740756653-deepgram-wordmark-black.png',
+  brandTarget: '_blank',
+});
---
+++ .storybook/manager.ts
@@ -0,0 +1,6 @@
+import { addons } from '@storybook/manager-api';
+import deepgramTheme from './deepgram';
+
+addons.setConfig({
+  theme: deepgramTheme,
+});
